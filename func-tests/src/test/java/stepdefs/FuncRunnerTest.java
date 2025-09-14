80311df981d099f11164fc0a8220e706857b42e0
lco47 <luke.cole@sky.uk>
2025-09-14 06:17:03 +0100

    [LC] remove junit due to cucumber conflicts

--- func-tests/src/test/java/stepdefs/FuncRunnerTest.java
+++ func-tests/src/test/java/stepdefs/FuncRunnerTest.java
@@ -1,14 +1,14 @@
-package stepdefs;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        plugin = {"pretty", "html:build/reports/tests/cucumber"},
-        features = "src/test/resources/features",
-        glue = "stepdefs"
-)
-public class FuncRunnerTest {
-}
+//package stepdefs;
+//
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        plugin = {"pretty", "html:build/reports/tests/cucumber"},
+//        features = "src/test/resources/features",
+//        glue = "stepdefs"
+//)
+//public class FuncRunnerTest {
+//}
