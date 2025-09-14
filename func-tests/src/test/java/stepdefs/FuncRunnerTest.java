b6a9f7741cf712024f91d478495d2480b72450f5
lco47 <luke.cole@sky.uk>
2025-09-14 06:07:19 +0100

    [LC] fix cucumber file

--- func-tests/src/test/java/stepdefs/FuncRunnerTest.java
+++ func-tests/src/test/java/stepdefs/FuncRunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:build/reports/tests/test"},
+        plugin = {"pretty", "html:build/reports/tests/cucumber"},
         features = "src/test/resources/features",
         glue = "stepdefs"
 )
