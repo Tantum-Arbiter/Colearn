cce1653a81df6009cb05e9627d3bfce997beca2e
lco47 <luke.cole@sky.uk>
2025-09-14 04:47:28 +0100

    [LC] fix cucumber issues

--- func-tests/src/test/java/stepdefs/FuncTestsRunner.java
+++ func-tests/src/test/java/stepdefs/FuncTestsRunner.java
@@ -1,8 +1,11 @@
 package stepdefs;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.runner.RunWith;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
