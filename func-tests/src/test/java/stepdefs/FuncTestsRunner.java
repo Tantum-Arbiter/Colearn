2d193084ff5c8f42025442557fd7261ff9dd0ce4
lco47 <luke.cole@sky.uk>
2025-09-14 04:42:42 +0100

    [LC] include cucumber options

--- func-tests/src/test/java/stepdefs/FuncTestsRunner.java
+++ func-tests/src/test/java/stepdefs/FuncTestsRunner.java
@@ -9,5 +9,11 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefs")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = {"pretty", "html:build/reports/cucumber.html"},
+    features = "src/test/resources/features",
+    glue = "com.app.func.steps"
+)
 public class FuncTestsRunner {
 }
