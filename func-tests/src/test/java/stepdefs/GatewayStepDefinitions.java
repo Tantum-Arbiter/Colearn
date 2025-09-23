333bb9ebe626d93ee2b76315e9edff77727f9e9c
lco47 <luke.cole@sky.uk>
2025-09-23 21:43:41 +0100

    [LC] add auth token

--- func-tests/src/test/java/stepdefs/GatewayStepDefinitions.java
+++ func-tests/src/test/java/stepdefs/GatewayStepDefinitions.java
@@ -16,15 +16,45 @@ public class GatewayStepDefinitions {
 
 private final String baseUrl = System.getenv().getOrDefault("GATEWAY_HOST", "http://gateway-service:8080");
 
+
+
+// RUN LOCAL/DOCKER COMPOSE
+//    @When("I GET {string}")
+//    public void i_get(String path) throws IOException, InterruptedException {
+//        System.out.println("*** >> URL is: " + baseUrl + path);
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://" + baseUrl + path))
+//                .GET()
+//                .build();
+//        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//    }
+
+    // push to master, run in GCP
     @When("I GET {string}")
     public void i_get(String path) throws IOException, InterruptedException {
-        System.out.println("*** >> URL is: " + baseUrl + path);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://" + baseUrl + path))
+        System.out.println("*** >> Base URL is: " + baseUrl);
+        System.out.println("*** >> Full URL is: " + baseUrl + path);
+
+        HttpRequest tokenRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=" + baseUrl))
+                .header("Metadata-Flavor", "Google")
                 .GET()
                 .build();
+
+        HttpResponse<String> tokenResponse = client.send(tokenRequest, HttpResponse.BodyHandlers.ofString());
+        String token = tokenResponse.body();
+
+        System.out.println("*** >> Acquired Identity Token: " + (token != null && !token.isEmpty() ? "TOKEN RECEIVED" : "NO TOKEN"));
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + path))
+                .header("Authorization", "Bearer " + token)
+                .GET()
+                .build();
+
         response = client.send(request, HttpResponse.BodyHandlers.ofString());
     }
+
 
     @Then("the response status should be {int}")
     public void the_response_status_should_be(Integer statusCode) {
