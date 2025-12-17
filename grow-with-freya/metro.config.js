ded83a1e73e7084a0e8dca8ec0779970ad3bd9e7
lco47 <luke.cole@sky.uk>
2025-12-17 22:14:47 +0000

    fix: update metro config for EAS build compatibility

--- grow-with-freya/metro.config.js
+++ grow-with-freya/metro.config.js
@@ -1,18 +1,17 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
 
-config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
-
-// Force Metro to resolve modules from local node_modules first
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(__dirname, '..', 'node_modules'),
-];
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...config.resolver.sourceExts, 'svg'],
+};
 
 module.exports = config;
 
