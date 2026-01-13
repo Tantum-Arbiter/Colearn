9545c4b3ef5c056903168b54e173df62af3bb5cf
lco47 <luke.cole@sky.uk>
2026-01-13 15:13:33 +0000

    Fix Android hot reload: Null out sound before module re-eval
    - Synchronously remove callback and null out sound reference
    - Prevents stale callbacks from firing during hot reload
    - Marks instance as not loaded so it reinitializes cleanly

--- grow-with-freya/services/background-music.ts
+++ grow-with-freya/services/background-music.ts
@@ -599,6 +599,28 @@ class BackgroundMusicService {
 declare global {
   // eslint-disable-next-line no-var
   var __backgroundMusicInstance: BackgroundMusicService | undefined;
+  // eslint-disable-next-line no-var
+  var __audioCleanupScheduled: boolean | undefined;
+}
+
+// On Android, schedule cleanup of the sound instance before hot reload
+// This must happen synchronously before the module is re-evaluated
+if (Platform.OS === 'android' && __DEV__ && global.__backgroundMusicInstance && !global.__audioCleanupScheduled) {
+  global.__audioCleanupScheduled = true;
+  // Synchronously null out the sound reference to prevent callbacks from firing
+  const instance = global.__backgroundMusicInstance as any;
+  if (instance.sound) {
+    // Remove the callback first (synchronous)
+    try {
+      instance.sound.setOnPlaybackStatusUpdate(null);
+    } catch {
+      // Ignore errors
+    }
+    // Mark as not loaded so it reinitializes
+    instance.isLoaded = false;
+    instance.sound = null;
+  }
+  global.__audioCleanupScheduled = false;
 }
 
 // Export singleton instance - reuse existing instance if available (survives hot reloads)
